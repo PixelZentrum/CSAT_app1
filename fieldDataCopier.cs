685311d560a85506802a96a4ac0181b742320ecc
Clofly Mao <greatmao@yandex.com>
2021-12-23 00:25:16 +0800

    添加项目文件。

---
+++ fieldDataCopier.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Workflow;
+
+namespace Dynamics365Plugin
+{
+    public class fieldDataCopier : CodeActivity
+
+    {
+        [RequiredArgument]
+        [Input("Pass your data here...")]
+        public InArgument<string> DataInput { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            ITracingService tracingService = context.GetExtension<ITracingService>();
+            tracingService.Trace("This will appear in the Trace Log");
+            try
+            {
+
+                string input = DataInput.Get(context);
+                IWorkflowContext workflowContext = context.GetExtension<IWorkflowContext>();
+                IOrganizationServiceFactory serviceFactory = context.GetExtension<IOrganizationServiceFactory>();
+                // Use the context service to create an instance of IOrganizationService.             
+                IOrganizationService service = serviceFactory.CreateOrganizationService(Guid.Parse("0B58A93E-B1E5-EB11-BACB-000D3A5ACB40"));
+                test1(input, workflowContext, service);
+                tracingService.Trace("Workflow executed successfully");
+            }
+            catch (Exception e)
+            {
+                throw new InvalidWorkflowException("The workflow failed with reason: " + e.Message);
+            }
+        }
+
+        private static void test1(String input, IWorkflowContext workflowContext, IOrganizationService service)
+        {
+            Guid recordId = workflowContext.PrimaryEntityId;
+            Entity updateentity = new Entity(workflowContext.PrimaryEntityName);
+            updateentity.Id = recordId;
+            updateentity["description"] = input;
+            service.Update(updateentity);
+        }
+    }
+}
---
+++ invoiceTest.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dynamics365Plugin
+{
+    public class invoiceTest : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            ITracingService tracingService = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+            try
+            {
+                
+                // Obtain the execution context from the service provider
+                IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+                IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+                IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+
+                // The InputParameters collection contains all the data passed in the message request.
+                if (context.InputParameters.Contains("Target") && context.InputParameters["Target"] is Entity)
+                {
+                    // Obtain the target entity from the input parameters.
+                    Entity entity = (Entity)context.InputParameters["Target"];
+
+                    if (entity.LogicalName == "invoice")
+                    {
+                        if (entity.Attributes.Contains("name") == true)
+                        {
+
+                            Entity updateentity = new Entity(entity.LogicalName);
+
+                            updateentity.Id = entity.Id;
+
+                            Random rd = new Random();
+                            int rand_num = rd.Next(100, 200);
+
+                            updateentity["description"] = rand_num.ToString();
+                            service.Update(updateentity);
+                        }
+                        else
+                        {
+                            throw new InvalidPluginExecutionException("Error occured.");
+                        }
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                tracingService.Trace("an error occurred...\n");
+                tracingService.Trace(ex.Message);
+                throw new InvalidPluginExecutionException(ex.Message);
+            }
+        }
+    }
+}
